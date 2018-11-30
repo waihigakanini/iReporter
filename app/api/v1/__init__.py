3fa9a31c5889192ed9d506e45dc298e97b88814c
waihiga <loycekarun@gmail>
2018-11-30 13:38:48 +0300

    [finishes #162327842 ] gets one redflag records

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint
 from flask_restful import Api, Resource
-from .views import RedFlags
+from .views import RedFlags, RedFlag 
 
 
 version_one = Blueprint ('v1', __name__, url_prefix='/api/v1')
@@ -9,4 +9,5 @@ version_one = Blueprint ('v1', __name__, url_prefix='/api/v1')
 api = Api(version_one)
 
 api.add_resource(RedFlags, '/red-flags')  
+api.add_resource(RedFlag, '/red-flags/<int:redflag_id>') 
 
--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -19,3 +19,40 @@ class RedFlags(Resource):
             "data" : self.db.get_all()
         }), 200) 
        
+    
+    def post(self):        
+        data = {
+            'createdOn' : datetime.datetime.utcnow(),
+            'createdBy' : request.json['createdBy'],
+            'type' : 'red-flags',
+            'location' : request.json['location'],
+            'status' : "Under Investigation",
+            # 'images' : request.json['images'],
+            # 'videos' : request.json['videos'],
+            'title' : request.json['title'],
+            'comment' : request.json['comment']
+        }
+        self.db.save(data)
+        
+        success_message = {
+            'message' : 'Thank You for Creating a Red-Flag'
+        }
+
+        return make_response(jsonify({
+            "status" : 201,
+            "data" : success_message
+        }), 201)
+    
+class RedFlag(Resource):
+    """docstring of RedFlag"""
+    def __init__(self):
+        self.db = RedFlagModel()
+        
+        # self.id = len(self.db) + 1
+    def get(self, redflag_id):
+
+        incident = self.db.find(redflag_id)
+        return make_response(jsonify({
+                    "status" : 200,
+                    "data" : incident
+                }), 200)
