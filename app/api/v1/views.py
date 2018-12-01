aade9b57980b2117bf48f692377715ae186d70ec
waihiga <loycekarun@gmail>
2018-12-01 06:52:58 +0300

    [Feature #162297465 ] Editing the contents of the Redflag Record file

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -68,51 +68,37 @@ class RedFlag(Resource):
         return make_response(jsonify({
             "status": 200,
             "data" : success_message}))
-    # def put(self, redflag_id):
-    #     data = {
-    #         'id' : redflag_id,
-    #         'createdOn' : datetime.datetime.utcnow(),
-    #         'createdBy' : request.json['createdBy'],
-    #         'type' : 'red-flags',
-    #         'location' : request.json['location'],
-    #         'status' : "Under Investigation",
-    #         # 'images' : request.json['images'],
-    #         # 'videos' : request.json['videos'],
-    #         'title' : request.json['title'],
-    #         'comment' : request.json['comment']
-    #     }
+    def put(self, redflag_id):
+        incident = self.db.find(redflag_id)
 
-    #     incident = self.db.find(redflag_id)
+        if incident:
+                     
+                     incident['location'] = request.json.get('location', incident['location'])
+                     incident['createdBy'] = request.json.get('createdBy', incident['createdBy'])
+                     incident['title'] = request.json.get('title', incident['title'])
+                     incident['comment'] = request.json.get('comment', incident['comment'])
+                     #incident['images'] = request.json.get('images', incident['images'])
+                     #incident['videos'] = request.json.get('videos', incident['videos'])
 
-    #     if incident:
+                     success_message = {
+                         "id" : redflag_id,
+                         "message" : "Updated red-flag record"
+                     } 
+                     return make_response(jsonify({
+                         "status" : 201,
+                         "data" : success_message
+                     }), 201)
+
             
 
-    #         incident['createdBy'] = request.json.get('createdBy', incident['createdBy'])
-    #         incident['location'] = request.json.get('location', incident['location'])
-    #         # incident['images'] = request.json.get('images', incident['images'])
-    #         # incident['videos'] = request.json.get('videos', incident['videos'])
-    #         incident['title'] = request.json.get('title', incident['title'])
-    #         incident['comment'] = request.json.get('comment', incident['comment'])
-
-    #         success_message = {
-    #             "id" : redflag_id,
-    #             "message" : "Red-flag has been updated"
-    #         }
-
-    #         return make_response(jsonify({
-    #             "status" : 201,
-    #             "data" : success_message
-    #         }), 201)
-    #     return make_response(jsonify({
-    #         "status" : 404,
-    #         "error" : "Red-flag does not exist"
-    #     }), 404)
-
+        else:
+            return {'error': 'Red flag does not exist', 'status': 404}
        
        
 
 
 class EditRedflagLocation(Resource):
+    """docstring of Update location of redflag"""
     def __init__(self):
         self.db = RedFlagModel()
         
@@ -137,6 +123,7 @@ class EditRedflagLocation(Resource):
         else:
             return {'error': 'Red flag does not exist', 'status': 404}
 class EditRedflagComment(Resource):
+    """docstring of update location for comment"""
     def __init__(self):
         self.db = RedFlagModel()
         
@@ -145,11 +132,11 @@ class EditRedflagComment(Resource):
 
         if incident:
                      
-                     incident['location'] = request.json.get('location', incident['location'])
+                     incident['comment'] = request.json.get('comment', incident['comment'])
 
                      success_message = {
                          "id" : redflag_id,
-                         "message" : "Updated red-flag records location"
+                         "message" : "Updated red-flag records comment"
                      } 
                      return make_response(jsonify({
                          "status" : 201,
